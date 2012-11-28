65e6c9b9957f72d679d9de15a470467bb9416258
Rob Mathers <robmathers@gmail.com>
2012-11-28 18:42:45 -0500

    Match and remove cookies

--- RemoveCookie/main.m
+++ RemoveCookie/main.m
@@ -8,15 +8,41 @@
 
 #import <Foundation/Foundation.h>
 
+// NSLog replacement from http://stackoverflow.com/a/3487392/1376063
+void IFPrint (NSString *format, ...) {
+    va_list args;
+    va_start(args, format);
+    
+    fputs([[[NSString alloc] initWithFormat:format arguments:args] UTF8String], stdout);
+    fputs("\n", stdout);
+    
+    va_end(args);
+}
+
 int main(int argc, const char * argv[])
 {
-
-    @autoreleasepool {
-        
-        // insert code here...
-        NSLog(@"Hello, World!");
-        
+    NSString *urlSearchString = nil;
+    if (argc > 1) {
+        urlSearchString = [NSString stringWithUTF8String:argv[1]];
     }
+    else {
+        IFPrint(@"No URL provided, quitting.");
+        return 1;
+    }
+    
+    NSHTTPCookieStorage *cookieStorage = [NSHTTPCookieStorage sharedHTTPCookieStorage];
+    
+    NSString *filterString = [NSString stringWithFormat:@"domain ENDSWITH '%@'", urlSearchString];
+    NSPredicate *filter = [NSPredicate predicateWithFormat:filterString];
+    
+    NSArray *matchedCookies = [cookieStorage.cookies filteredArrayUsingPredicate:filter];
+
+    for (int i = 0; i < matchedCookies.count; i++) {
+        [cookieStorage deleteCookie:[matchedCookies objectAtIndex:i]];
+    }
+    
+    IFPrint(@"Removed %li cookies", matchedCookies.count);
     return 0;
+        
 }
 
