646c0bbfcf4b49fa5771d65372f17b8f618bfd0c
Rob Mathers <robmathers@gmail.com>
2012-11-28 16:53:11 -0500

    Initial Commit

---
+++ RemoveCookie/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  RemoveCookie
+//
+//  Created by Rob Mathers on 12-11-28.
+//  Copyright (c) 2012 Rob Mathers. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
